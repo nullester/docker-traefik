eb51feedc6b1ed5c1ec599385c435e09a07e65b4
nullester <65467511+nullester@users.noreply.github.com>
2023-04-17 21:15:04 +0200

    .env

--- start.sh
+++ start.sh
@@ -5,44 +5,62 @@ if [[ -f $V_ROOT/.env ]]; then
     . $V_ROOT/.env
 fi
 
-V_ENV="$1"
+V_ENV=${ENVIRONMENT:-web}
 if [[ "$V_ENV" == "" ]]; then
     V_ENV="web"
 fi
 
 echo
-echo -e "\033[036m┌───────────────────────────────────────────────────────────\033[0m"
+echo -e "\033[036m┌─────────────────────────────────────────────────────────────────────────────\033[0m"
 
 echo -e "\033[036m│\033[0m"
-echo -e "\033[036m│\033[0m Starting the \033[036mTraefik\033[0m container"
+echo -e "\033[036m│\033[0m Starting the \033[036mTraefik\033[0m container with the \033[93m${V_ENV}\033[0m environment"
 echo -e "\033[036m│\033[0m"
 
-if [[ "$V_ENV" == "web" && ! -f acme.json ]]; then
-    echo -e "\033[036m│\033[0m Creating \033[036macme.json\033[0m"
-    touch acme.json
-    chmod 0600 acme.json
-    ls -la --color | grep --color=always "acme.json"
+if [[ "$V_ENV" == "web" && ! -f "$V_ROOT/conf/acme.json" ]]; then
+    echo -e "\033[036m│\033[0m Creating \033[036mconf/acme.json\033[0m"
+    touch "$V_ROOT/conf/acme.json"
+    if [[ ! -f "$V_ROOT/conf/acme.json" ]]; then
+        echo -e "\033[036m│\033[0m"
+        echo -e "\033[036m│\033[0m \033[031mError: creating $V_ROOT/conf/acme.json failed, aborting\033[0m"
+        echo -e "\033[036m│\033[0m"
+        echo -e "\033[036m└─────────────────────────────────────────────────────────────────────────────\033[0m"
+        exit 1
+    fi
+    chmod 0600 "$V_ROOT/conf/acme.json"
+    ls -la --color "$V_ROOT/conf"| grep --color=always "acme.json"
+    echo -e "\033[036m│\033[0m"
+    echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
     echo -e "\033[036m│\033[0m"
 fi
 
 echo -e "\033[036m│\033[0m Current \033[036mTraefik\033[0m containers"
 docker ps -a | grep --color=always "traefik"
+
+echo -e "\033[036m│\033[0m"
+echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo -e "\033[036m│\033[0m"
 
 echo -e "\033[036m│\033[0m Creating the Docker \033[036mweb\033[0m network"
 docker network create web
+
+echo -e "\033[036m│\033[0m"
+echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo -e "\033[036m│\033[0m"
 
 echo -e "\033[036m│\033[0m Starting the \033[036mTraefik\033[0m container"
-docker compose up "traefik-${V_ENV}" -d
+docker compose up "traefik" -d
 docker ps -a | grep "traefik"
+
+echo -e "\033[036m│\033[0m"
+echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo -e "\033[036m│\033[0m"
 
 echo -e "\033[036m│\033[0m Current \033[036mTraefik\033[0m containers after start"
 docker ps -a | grep --color=always "traefik"
-echo -e "\033[036m│\033[0m"
 
-echo -e "\033[036m└───────────────────────────────────────────────────────────\033[0m"
+echo -e "\033[036m│\033[0m"
+echo -e "\033[036m└─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo
 
 exit 0
--- stop.sh
+++ stop.sh
@@ -9,36 +9,43 @@ echo
 echo -e "\033[036m┌───────────────────────────────────────────────────────────\033[0m"
 
 echo -e "\033[036m│\033[0m"
-echo -e "\033[036m│\033[0m Stopping all \033[036mTraefik\033[0m containers"
-echo -e "\033[036m│\033[0m"
-
 echo -e "\033[036m│\033[0m Current \033[036mTraefik\033[0m containers"
 docker ps -a | grep --color=always "traefik"
+
+echo -e "\033[036m│\033[0m"
+echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo -e "\033[036m│\033[0m"
 
+echo -e "\033[036m│\033[0m Stopping \033[036mTraefik\033[0m containers"
 docker stop $( docker ps -a | grep "traefik" | awk '{print $1}' )
 echo -e "\033[036m│\033[0m"
-
 echo -e "\033[036m│\033[0m Current \033[036mTraefik\033[0m containers after stop"
 docker ps -a | grep --color=always "traefik"
+
+echo -e "\033[036m│\033[0m"
+echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo -e "\033[036m│\033[0m"
 
 echo -e "\033[036m│\033[0m Removing \033[036mTraefik\033[0m containers"
 docker rm $( docker ps -a -f status=exited -f status=created | grep "traefik" | awk '{print $1}' )
 echo -e "\033[036m│\033[0m"
-
 echo -e "\033[036m│\033[0m Current \033[036mTraefik\033[0m containers after remove"
 docker ps -a | grep --color=always "traefik"
+
+echo -e "\033[036m│\033[0m"
+echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo -e "\033[036m│\033[0m"
 
 echo -e "\033[036m│\033[0m Current \033[036mTraefik\033[0m images"
 docker image ls | grep --color=always "traefik"
+
+echo -e "\033[036m│\033[0m"
+echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
 echo -e "\033[036m│\033[0m"
 
 echo -e "\033[036m│\033[0m Removing \033[036mTraefik\033[0m images"
 docker rmi $( docker image ls | grep --color=always "traefik" | awk '{print $3}' )
 echo -e "\033[036m│\033[0m"
-
 echo -e "\033[036m│\033[0m Current \033[036mTraefik\033[0m images after remove"
 docker image ls | grep --color=always "traefik"
 echo -e "\033[036m│\033[0m"
