f005033bd3fa1c2d591496d137e584e82b45b1ab
nullester <65467511+nullester@users.noreply.github.com>
2023-05-02 14:37:21 +0200

    Create toml files on startup added

--- includes.sh
+++ includes.sh
@@ -1,33 +1,51 @@
 #!/usr/bin/env bash
 
-# V_ROOT=$( dirname $( readlink -f "$0" ) ) # @todo: doesn't work on Mac
-V_ROOT="."
-if [[ -f $V_ROOT/.env ]]; then . $V_ROOT/.env; fi
+V_ROOT=$( dirname $( readlink -f "$0" ) )
+if [[ -f $V_ROOT/.env ]]; then
+    . $V_ROOT/.env
+fi
 V_ENV=${ENVIRONMENT:-web}
-if [[ "$V_ENV" == "" ]]; then V_ENV="web"; fi
+if [[ "$V_ENV" == "" ]]; then
+    V_ENV="web"
+fi
 
-function F_DRAW_START() {
+function F_TFK_TRAP() {
+    local V_MSG="$1"
+    echo
+    F_TFK_DRAW_BREAK
+    if [[ -n "V_MSG" ]]; then
+        F_TFK_DRAW_OUT "\033[031mAborted\033[0m"
+    else
+        F_TFK_DRAW_OUT "\033[031m${V_MSG}\033[0m"
+    fi
+    F_TFK_DRAW_END
+    exit 1
+}
+
+trap F_TFK_TRAP SIGINT
+
+function F_TFK_DRAW_START() {
     echo
     echo -e "\033[036m┌─────────────────────────────────────────────────────────────────────────────\033[0m"
     echo -e "\033[036m│\033[0m"
 }
 
-function F_DRAW_BREAK() {
+function F_TFK_DRAW_BREAK() {
     echo -e "\033[036m│\033[0m"
 }
 
-function F_DRAW_RULER() {
+function F_TFK_DRAW_RULER() {
     echo -e "\033[036m│\033[0m"
     echo -e "\033[036m├─────────────────────────────────────────────────────────────────────────────\033[0m"
     echo -e "\033[036m│\033[0m"
 }
 
-function F_DRAW_END() {
+function F_TFK_DRAW_END() {
     echo -e "\033[036m│\033[0m"
     echo -e "\033[036m└─────────────────────────────────────────────────────────────────────────────\033[0m"
     echo
 }
 
-function F_DRAW_OUT() {
+function F_TFK_DRAW_OUT() {
     echo -e "\033[036m│\033[0m $1"
 }
--- stop.sh
+++ stop.sh
@@ -2,41 +2,41 @@
 
 . includes.sh
 
-F_DRAW_START
+F_TFK_DRAW_START
 
-F_DRAW_OUT "Current \033[036mTraefik\033[0m containers"
+F_TFK_DRAW_OUT "Current \033[036mTraefik\033[0m containers"
 docker ps -a | grep --color=always "traefik"
 
-F_DRAW_RULER
+F_TFK_DRAW_RULER
 
-F_DRAW_OUT "Stopping \033[036mTraefik\033[0m containers"
+F_TFK_DRAW_OUT "Stopping \033[036mTraefik\033[0m containers"
 docker stop $( docker ps -a | grep "traefik" | awk '{print $1}' )
-F_DRAW_BREAK
-F_DRAW_OUT "Current \033[036mTraefik\033[0m containers after stop"
+F_TFK_DRAW_BREAK
+F_TFK_DRAW_OUT "Current \033[036mTraefik\033[0m containers after stop"
 docker ps -a | grep --color=always "traefik"
 
-F_DRAW_RULER
+F_TFK_DRAW_RULER
 
-F_DRAW_OUT "Removing \033[036mTraefik\033[0m containers"
+F_TFK_DRAW_OUT "Removing \033[036mTraefik\033[0m containers"
 docker rm $( docker ps -a -f status=exited -f status=created | grep "traefik" | awk '{print $1}' )
-F_DRAW_BREAK
-F_DRAW_OUT "Current \033[036mTraefik\033[0m containers after remove"
+F_TFK_DRAW_BREAK
+F_TFK_DRAW_OUT "Current \033[036mTraefik\033[0m containers after remove"
 docker ps -a | grep --color=always "traefik"
 
-F_DRAW_RULER
+F_TFK_DRAW_RULER
 
-F_DRAW_OUT "Current \033[036mTraefik\033[0m images"
+F_TFK_DRAW_OUT "Current \033[036mTraefik\033[0m images"
 docker image ls | grep --color=always "traefik"
 
-F_DRAW_RULER
+F_TFK_DRAW_RULER
 
-F_DRAW_OUT "Removing \033[036mTraefik\033[0m images"
+F_TFK_DRAW_OUT "Removing \033[036mTraefik\033[0m images"
 docker rmi $( docker image ls | grep --color=always "traefik" | awk '{print $3}' )
-F_DRAW_BREAK
-F_DRAW_OUT "Current \033[036mTraefik\033[0m images after remove"
+F_TFK_DRAW_BREAK
+F_TFK_DRAW_OUT "Current \033[036mTraefik\033[0m images after remove"
 docker image ls | grep --color=always "traefik"
-F_DRAW_BREAK
+F_TFK_DRAW_BREAK
 
-F_DRAW_OUT
+F_TFK_DRAW_OUT
 
 exit 0
